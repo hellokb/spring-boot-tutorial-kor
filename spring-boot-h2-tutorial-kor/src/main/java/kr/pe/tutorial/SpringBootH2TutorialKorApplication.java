a16c255d00db4a1f3d2f8b651976b83ea3d73442
hellokb <mymetelsos@gmail.com>
2020-12-09 16:46:00 +0900

    initial commit

---
+++ spring-boot-h2-tutorial-kor/src/main/java/kr/pe/tutorial/SpringBootH2TutorialKorApplication.java
@@ -0,0 +1,13 @@
+package kr.pe.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2TutorialKorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2TutorialKorApplication.class, args);
+	}
+
+}
---
+++ spring-boot-h2-tutorial-kor/src/test/java/kr/pe/tutorial/SpringBootH2TutorialKorApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pe.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootH2TutorialKorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
